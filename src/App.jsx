7fed5ac94582c2b08eb15ebc396de3587bc00ccd
Agustina Mena <agusmena60@gmail.com>
2026-03-02 10:12:58 -0300

    feat: instalo tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+   <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-pink-500">
+        Tailwind funciona 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
