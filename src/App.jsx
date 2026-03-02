8907f1001d684b701bdfe56112978e9fdb395da6
Agustina Mena <agusmena60@gmail.com>
2026-03-02 10:45:01 -0300

    feat: estructura de carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-base-100 gap-6">
-      <h1 className="text-4xl font-bold text-primary">
-        Tailwind funciona 🎉
-      </h1>
-
-      <button className="btn btn-primary">
-        DaisyUI funciona 🌼
-      </button>
-         <button className="btn btn-primary">
-        Primary
-      </button>
-    </div>
+   <div>
+    
+   </div>
   );
 }
 
