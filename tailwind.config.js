c1d18a5b93e9c5a57f451e814324820a00299cb7
Agustina Mena <agusmena60@gmail.com>
2026-03-09 17:48:11 -0300

    fix: confihuro el theme para que en modo oscuro use el tema corparate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: ["corporate"],
+    darkTheme: "corporate",
+  },
 };
